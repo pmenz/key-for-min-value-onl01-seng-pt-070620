7d9c90e3539e1985632cd83bdff0b102e853814a
Pablo Menz <p.menz.r@gmail.com>
2020-07-26 04:22:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,3 +11,15 @@ name_hash.each do |name, value|
                end
 
 end
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
